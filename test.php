638cbf91956c37504c5a5858c3e4946af38eb5d9
tsli <2816869721@qq.com>
2020-10-14 15:29:35 +0800

    ''

--- test.php
+++ test.php
@@ -3,12 +3,6 @@ Co::set(['hook_flags'=> SWOOLE_HOOK_ALL]);
 
 Co\run(function () {
     $client = new Swoole\Coroutine\Http\Client('wiki.swoole.com', 80);
-    $client->setHeaders([
-        'Host' => 'localhost',
-        'User-Agent' => 'Chrome/49.0.2587.3',
-        'Accept' => 'text/html,application/xhtml+xml,application/xml',
-        'Accept-Encoding' => 'gzip',
-    ]);
     $client->set(['timeout' => 1]);
     $client->get('/');
     echo $client->body;
