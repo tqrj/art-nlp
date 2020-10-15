5c2bebf7fa0b7519dede1aaa61289bd1cb8e8a93
tsli <2816869721@qq.com>
2020-10-15 19:22:20 +0800

    ''

--- nlp/Baidu.php
+++ nlp/Baidu.php
@@ -57,7 +57,7 @@ class Baidu implements NlpInterface,NlpBaiduInterface
     {
         // TODO: Implement dnnlmCn() method.
         //$data['access_token'] = $this->config['baidu']['access_token'];
-        $data['text'] = $text;
+        $data['text'] = iconv('UTF-8','GBK',$text);
         $data = json_encode($data);
         echo $data;
         $client = new Client(self::BaiduDomain,443,true);
