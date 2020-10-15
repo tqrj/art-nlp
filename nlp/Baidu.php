96057b5d1823ff7a6c368bb293fd181641ff08b8
tsli <2816869721@qq.com>
2020-10-15 15:08:07 +0800

    ''

--- nlp/Baidu.php
+++ nlp/Baidu.php
@@ -23,16 +23,17 @@ class Baidu implements NlpInterface,NlpBaiduInterface
     {
         // TODO: Implement GetAccessToken() method.
         $body = '';
-        go(function () use (&$body){
-            $data['client_secret'] = $this->config['baidu']['client_secret'];
-            $data['client_id'] = $this->config['baidu']['client_id'];
-            $data['grant_type'] = 'client_credentials';
-            $client = new Client('aip.baidubce.com',80);
-            $client->set(['timeout'=>$this->config['outTime']]);
-            $client->post('/oauth/2.0/token',$data);
-            $body = $client->getBody();
-            $client->close();
-        });
+//        go(function () use (&$body){
+//
+//        });
+        $data['client_secret'] = $this->config['baidu']['client_secret'];
+        $data['client_id'] = $this->config['baidu']['client_id'];
+        $data['grant_type'] = 'client_credentials';
+        $client = new Client('aip.baidubce.com',80);
+        $client->set(['timeout'=>$this->config['outTime']]);
+        $client->post('/oauth/2.0/token',$data);
+        $body = $client->getBody();
+        $client->close();
         return json_decode($body,true)['access_token'];
     }
 
